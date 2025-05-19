2ca1c0d6d407c83055dad82b99c5fce4a45feceb
Joao Soares <joazinhovitorsoares@gmail.com>
2025-05-19 12:16:41 -0300

    Teste acabado 009

---
+++ Typescript/main.js
@@ -0,0 +1,20 @@
+var minhaVar = 'minha variavel';
+function minhaFunca(x, y) {
+    return x + y;
+}
+var nume = 2;
+var Pii = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
+var Matematic = /** @class */ (function () {
+    function Matematic() {
+    }
+    Matematic.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    ;
+    return Matematic;
+}());
